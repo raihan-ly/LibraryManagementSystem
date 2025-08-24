1eb5d67b7386934f96cdbd76f08e3fcf0d383e60
Raihan <raihan@Mohammeds-MacBook-Air.local>
2025-08-24 16:31:36 +0530

    refactor: replace arrays with arraylist in LibraryService

--- src/service/LibraryService.java
+++ src/service/LibraryService.java
@@ -2,42 +2,36 @@ package service;
 
 import model.Book;
 
+import java.util.ArrayList;
+
 public class LibraryService {
 
-    private Book[] books = new Book[100];
-    private int bookCount = 0;
+    private ArrayList<Book> books = new ArrayList<>();
     private int nextId = 1;
 
     public void addBook(String title, String author, String category) {
-        if (bookCount >= books.length) {
-            System.out.println("Library is full! Can't add more books.");
-            return;
-        }
         Book addedBook = new Book(nextId++, title, author, category, true);
-        books[bookCount++] = addedBook;
+        books.add(addedBook);
         System.out.println("Book added: " + addedBook);
     }
 
     public void listBooks() {
-        if (bookCount == 0) {
-            System.out.println("No books available in the Library.");
+        if (books.isEmpty()) {
+            System.out.println("Library is empty");
             return;
         }
-        System.out.println("Library Books: ");
-        for(int i = 0; i <=books.length; i++) {
-            if(books[i] == null) {
-                break;
-            }
-            System.out.println(books[i]);
+        System.out.println("Library Books:");
+        for (Book book: books) {
+            System.out.println(book);
         }
     }
-
+    
     public void borrowBook(int id) {
-        for (int i = 0; i < bookCount; i++) {
-            if (books[i].getId() == id) {
-                if (books[i].isAvailable()) {
-                    books[i].setAvailable(false);
-                    System.out.println("Borrowed Book: " + books[i].getTitle());
+        for (Book book: books) {
+            if (book.getId() == id) {
+                if (book.isAvailable()) {
+                    book.setAvailable(false);
+                    System.out.println("Borrowed Book: " + book.getTitle());
                 }
                 else {
                     System.out.println("Book is already borrowed.");
@@ -49,11 +43,11 @@ public class LibraryService {
     }
 
     public void returnBook(int id) {
-        for(int i = 0; i < bookCount; i++) {
-            if(books[i].getId() == id) {
-                if (!books[i].isAvailable()) {
-                    books[i].setAvailable(true);
-                    System.out.println("Book returned: " + books[i].getTitle());
+        for (Book book: books) {
+            if (book.getId() == id) {
+                if (!book.isAvailable()) {
+                    book.setAvailable(true);
+                    System.out.println("Book returned: " + book.getTitle());
                 }
                 else {
                     System.out.println("Book is available and hasn't been borrowed");
@@ -66,9 +60,10 @@ public class LibraryService {
 
     public void searchBook(String keyword) {
         boolean found = false;
-        for (int i = 0; i < bookCount; i++) {
-            if (books[i].getTitle().toLowerCase().contains(keyword.toLowerCase())){
-                System.out.println("Book found: " + books[i].getTitle());
+        String lowerKeyword = keyword.toLowerCase();
+        for (Book book: books) {
+            if (book.getTitle().toLowerCase().contains(lowerKeyword)){
+                System.out.println("Book found: " + book.getTitle());
                 found = true;
             }
         }
@@ -77,4 +72,5 @@ public class LibraryService {
         }
     }
 }
+
 
